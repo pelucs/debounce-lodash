1e1030c9f0eb640f616d09fc318e49c305f8c286
Pedro Lucas <88797361+pelucs@users.noreply.github.com>
2022-02-24 18:04:36 -0300

    Add files via upload

---
+++ debounce.js
@@ -0,0 +1,19 @@
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+window.addEventListener("scroll", debounce(function(){
+  //Sua função
+  //function(){}
+}, 200));
